9dff268b4315c94b19ceb14d0b55e934eb71f33d
4709-boop <bettymont66@yahoo.com>
2026-07-30 16:37:59 -0600

    save everything

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 // template_fodbta9
 // service_ok2u9az
 // to3dd1XMrMf94YGxy
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading')
+      const success = document.querySelector('.modal__overlay--success')
+      loading.classList += " modal__overlay--visible";
+
+
+     emailjs
+     .sendForm(
+      'service_ok2u9az',
+     'template_fodbta9',
+      event.target,
+       'to3dd1XMrMf94YGxy'
+      ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+             
+    }
+
+    let isModalOpen = false;
+    function toggleModal() {
+        if (isModalOpen) {
+          isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+      }
+       isModalOpen = true;
+       // toggle modal
+      document.body.classList += " modal--open";
+    }
